25b838e6cc3f0ee48e14b088cf85512cb1d81f08
kmfiguerrez <kmfiguerrez@gmail.com>
2024-02-06 16:51:47 +0800

    Create errorcard comp.
    - Added /auth/error entry in the authRoutes in
      route.ts file.

--- src/app/auth/error/page.tsx
+++ src/app/auth/error/page.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import ErrorCard from '@/components/auth/error-card'
 
 const ErrorPage = () => {
   return (
-    <div>ErrorPage</div>
+    <ErrorCard />
   )
 }
 
---
+++ src/components/auth/error-card.tsx
@@ -0,0 +1,25 @@
+import Header from './header'
+import BackButton from './back-button'
+import {
+  Card,
+  CardFooter,
+  CardHeader
+} from "@/components/ui/card"
+
+const ErrorCard = () => {
+  return (
+    <Card className='w-[400px] shadow-md'>
+      <CardHeader>
+        <Header label='Something went wrong!' />
+      </CardHeader>
+      <CardFooter>
+        <BackButton
+          label='Back to login'
+          href='/auth/login'
+        />
+      </CardFooter>
+    </Card>
+  )
+}
+
+export default ErrorCard
