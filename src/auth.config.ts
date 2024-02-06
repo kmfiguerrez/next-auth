c79a399541279363f17eac509f90efa67e3ec31a
kmfiguerrez <kmfiguerrez@gmail.com>
2024-02-06 10:35:18 +0800

    Add oauth providers.
    - Added github and google providers.
    - Added entries for each providers.

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,5 +1,7 @@
 import type { NextAuthConfig } from "next-auth"
 import Credentials from "next-auth/providers/credentials"
+import github from "next-auth/providers/github"
+import google from "next-auth/providers/google"
 
 import LoginSchema from "./schemas/login-form"
 import { getUserByEmail } from "./data/user"
@@ -9,6 +11,14 @@ import bcrypt from "bcryptjs"
 
 export default {
   providers: [
+    github({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+    google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
     Credentials({
       async authorize(credentials) {
         console.log("credentials: ", credentials)
