ff365a75d0a6dd6db3c1127eb68a11b173abc104
kmfiguerrez <kmfiguerrez@gmail.com>
2024-02-19 14:43:24 +0800

    Add validation on server actions.
    - Added register and login validation on server
      actions.

--- src/actions/login.ts
+++ src/actions/login.ts
@@ -1,9 +1,16 @@
 "use server"
 
 import { TLoginSchema } from "@/schemas/login-schema"
+import RegisterSchema from "@/schemas/register-schema"
 
 
 export const login = async (values: TLoginSchema) => {
+  // Validate form values because server actions run on the server.
+  const validatedFields = RegisterSchema.safeParse(values)
+
+  if (!validatedFields.success) {
+    return { error: "Invalid fields!" }
+  }
 
   await new Promise((resolve, reject) => {
     setTimeout(() => resolve("success"), 5000)
--- src/actions/register.ts
+++ src/actions/register.ts
@@ -1,8 +1,15 @@
 "use server"
 
+import LoginSchema from "@/schemas/login-schema"
 import { type TRegisterSchema } from "@/schemas/register-schema"
 
 export const register = async (values: TRegisterSchema) => {
+  // Validate form values because server actions run on the server.
+  const validatedFields = LoginSchema.safeParse(values)
+
+  if (!validatedFields.success) {
+    return { error: "Invalid fields!"}
+  }
 
   await new Promise((resolve, reject) => {
     setTimeout(() => resolve("success"), 5000)
